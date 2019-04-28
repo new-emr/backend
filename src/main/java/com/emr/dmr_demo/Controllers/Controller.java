9948cd0b4324e41df10175d6b01a98225404a7b7
Hugh <deng.ch@northeastern.edu>
2019-04-27 23:43:10 -0700

    optimized import

--- src/main/java/com/emr/dmr_demo/Controllers/Controller.java
+++ src/main/java/com/emr/dmr_demo/Controllers/Controller.java
@@ -5,6 +5,8 @@ import com.emr.dmr_demo.Entities.Feature;
 import com.emr.dmr_demo.Entities.Patient;
 import com.emr.dmr_demo.Repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/emr/dmr_demo/Repositories/PatientRepository.java
+++ src/main/java/com/emr/dmr_demo/Repositories/PatientRepository.java
@@ -3,7 +3,5 @@ package com.emr.dmr_demo.Repositories;
 import com.emr.dmr_demo.Entities.Patient;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.UUID;
-
 public interface PatientRepository extends CrudRepository<Patient, Long> {
 }
